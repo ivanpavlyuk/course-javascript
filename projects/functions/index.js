18ba62f9441cd8e8c10683f11addb1343010d431
Ivan Pavlyuk <ivanpavlyuk30071989@gmail.com>
2024-02-13 22:03:02 +0300

    Сделал задачи недели#1

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,59 +12,70 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
-
+function returnFirstArgument(a) {
+  return a;
+}
 /*
  Задание 2:
-
+ 
  2.1: Функция должна возвращать сумму переданных аргументов
-
+ 
  Пример:
    sumWithDefaults(10, 20) вернет 30
    sumWithDefaults(2, 4) вернет 6
-
+ 
  2.2 *: Значение по умолчанию для второго аргумента должно быть равно 100
-
+ 
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 
 /*
  Задание 3:
-
+ 
  Функция должна принимать другую функцию и возвращать результат вызова этой функции
-
+ 
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(otherFunction) {
+  return otherFunction();
+}
 
 /*
  Задание 4:
-
+ 
  Функция должна принимать число и возвращать новую функцию (F)
  При вызове функции F, переданное ранее число должно быть увеличено на единицу и возвращено из F
-
+ 
  Пример:
    var f = returnCounter(10);
-
+ 
    console.log(f()); // выведет 11
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(number = 0) {
+  return function () {
+    return ++number;
+  };
+}
 
 /*
  Задание 5 *:
-
+ 
  Функция должна возвращать все переданные ей аргументы в виде массива
  Количество переданных аргументов заранее неизвестно
-
+ 
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...params) {
+  return params;
+}
 
 export {
   returnFirstArgument,
