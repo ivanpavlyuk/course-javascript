5148d59ab1f97f6aaa45bd68ac5b51ec7b9c5afb
Ivan Pavlyuk <ivanpavlyuk30071989@gmail.com>
2024-02-20 04:06:46 +0300

    Вернул файл в рабочее состояние

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -3,7 +3,24 @@ import photosDB from './photos.json';
 // eslint-disable-next-line no-unused-vars
 import friendsDB from './friends.json';
 
+// Для тестов
+// const photosDB = require('./photos.json');
+// const friendsDB = require('./friends.json');
+
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    const index = parseInt(Math.random() * array.length);
+    return array[index];
+  },
+  getNextPhoto() {
+    const index = parseInt(Math.random() * friendsDB.length);
+    const friend = friendsDB[index];
+
+    const photoIndex = parseInt(Math.random() * photosDB[index].length);
+    const photo = photosDB[index][photoIndex];
+    return {
+      friend,
+      url: photo.url,
+    };
+  },
 };
